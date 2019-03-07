7d85e4072efa603e3598c1f8a982c1a7262a471a
David Corwin <dshvazz@gmail.com>
2019-03-06 22:53:46 -0800

    Add tailwind extractor

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,26 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 const plugins = [
   require('tailwindcss')
 ];
 
 if (process.env.NODE_ENV === 'production') {
+  const purgecss = require('@fullhuman/postcss-purgecss')
+
+  class TailwindExtractor {
+    static extract(content) {
+      return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+  }
+
   plugins.push(require('autoprefixer'));
-  plugins.push(purgecss({ content: ['./index.html'] }));
+  plugins.push(purgecss({
+    content: ['./index.html'],
+    extractors: [
+      {
+        extractor: TailwindExtractor,
+        extensions: ["html"]
+      }
+    ]
+  }));
 }
 
 module.exports = {
