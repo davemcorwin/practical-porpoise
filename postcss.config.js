a00102a55e9c7cf8e38df53b39274e1253210c12
David Corwin <dshvazz@gmail.com>
2019-03-06 22:26:36 -0800

    Tailwind ftw

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
 
+const plugins = [
+  require('tailwindcss')
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(require('autoprefixer'));
+  plugins.push(purgecss({ content: ['./index.html'] }));
+}
+
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-    // purgecss({
-    //   content: ['./index.html']
-    // })
-  ]
+  plugins
 }
