ed1796c14dce822f4108d866dbcce1c1afe77fa5
David Corwin <david.corwin@gsa.gov>
2019-03-07 19:10:20 -0800

    Tailwind (#1)
    Add tailwind, postcss, redesign

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+const tailwind = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+const plugins = [
+  tailwind('./tailwind.js'),
+  purgecss({
+    content: ['./index.html'],
+    extractors: [
+      {
+        extractor: TailwindExtractor,
+        extensions: ['html']
+      }
+    ]
+  }),
+  require('autoprefixer')
+];
+
+module.exports = {
+  plugins
+}
